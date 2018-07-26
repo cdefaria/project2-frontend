2cb00adc40d3df58acce74c37d9d0a408a95345d
Karen Matney <matneyka@gmail.com>
2018-07-26 14:23:00 -0400

    Adding back-end url to environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,6 +14,7 @@ import { TrendingComponent } from './trending/trending.component';
 import { RateComponent } from './rate/rate.component';
 import { ShareComponent } from './share/share.component';
 import { CommentComponent } from './comment/comment.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
@@ -25,10 +28,12 @@ import { CommentComponent } from './comment/comment.component';
     TrendingComponent,
     RateComponent,
     ShareComponent,
-    CommentComponent
+    CommentComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
