ce8608ad46878d914516d85a55a3b564c287fdaa
Christian <cdefaria@mail.csuchico.edu>
2018-07-26 09:14:19 -0400

    Adding basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { InterestsComponent } from './interests/interests.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { TrendingComponent } from './trending/trending.component';
+import { RateComponent } from './rate/rate.component';
+import { ShareComponent } from './share/share.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    InterestsComponent,
+    SearchComponent,
+    HomeComponent,
+    FavoritesComponent,
+    TrendingComponent,
+    RateComponent,
+    ShareComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
