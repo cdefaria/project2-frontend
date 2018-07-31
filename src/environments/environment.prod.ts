83fe74337d586c224be1d7bd83cbd12247bdc04b
Karen Matney <matneyka@gmail.com>
2018-07-31 10:01:16 -0400

    Adding values to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "http://scoopsharebackendeb-env.cxgupqpebh.us-east-1.elasticbeanstalk.com/"
+    apiUrl: "http://scoopsharebackendeb-env.cxgupqpebh.us-east-1.elasticbeanstalk.com/",
+    newsApiUrl: "https://newsapi.org/v2/",
+    apiKey: "ae2f706bf18347bfa47933315a9dd514"
 };
