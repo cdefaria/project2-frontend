19f315898626001d835a09de6703ef258a06646b
Christian <cdefaria@mail.csuchico.edu>
2018-08-28 15:02:01 -0400

    Added some CSS, and minor redesigns for some pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://scoopsharebackendeb-env.cxgupqpebh.us-east-1.elasticbeanstalk.com/",
+  // apiUrl: "http://scoopsharebackendeb-env.cxgupqpebh.us-east-1.elasticbeanstalk.com/",
+  apiUrl: "http://localhost:8080/ScoopShare/",
   newsApiUrl: "https://newsapi.org/v2/",
   apiKey: "ae2f706bf18347bfa47933315a9dd514"
 };
